af30509f59e1309f26fc391d9c71635cfdbfa0cc
Arhacai <arhacai@gmail.com>
2018-09-28 20:06:40 +0200

    Created organizer and blog models, added requirements to project

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from organizer.models import Startup, Tag
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        max_length=35,
+        help_text='A label for URL config',
+        unique_for_month='pub_date'
+    )
+    text = models.TextField()
+    pub_date = models.DateField('date publised', auto_now_add=True)
+
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, 'blog_posts')
+
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
+
+    def __str__(self):
+        return "{} on {}".format(
+            self.title, self.pub_date.strftime('%Y-%m-%d')
+        )

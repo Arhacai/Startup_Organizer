b8edaa297e140950e928c243540c9e2d655c3ae9
Arhacai <arhacai@gmail.com>
2018-09-30 16:27:03 +0200

    Changed basic views to use render instead of render_to_response

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,5 +1,16 @@
-from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Tag
 
 
 def homepage(request):
-    return HttpResponse("Hello (again) World!")
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()}
+    )
+
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(request, 'organizer/tag_detail.html', {'tag': tag})
